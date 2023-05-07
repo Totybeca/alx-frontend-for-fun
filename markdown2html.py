167f68ccf7ee0a5ac275fdf67fa27b44bf281bc2
Totybeca <emeraldtoyin@gmail.com>
2023-05-08 00:28:08 +0100

    frontend is fun

--- markdown2html.py
+++ markdown2html.py
@@ -1,336 +1,43 @@
 #!/usr/bin/python3
+'''
+This script codes the markdown to HTML
+'''
 import sys
 import os
-import markdown
-
-
-def convert_markdown_to_html(markdown_file, output_file):
-    # Check if the Markdown file exists
-    if not os.path.exists(markdown_file):
-        print(f"Missing {markdown_file}", file=sys.stderr)
-        sys.exit(1)
-
-    # Read the contents of the Markdown file
-    with open(markdown_file, "r") as f:
-        markdown_text = f.read()
-
-    # Convert Markdown to HTML
-    html_text = markdown.markdown(markdown_text)
-
-    # Write the HTML output to a file
-    with open(output_file, "w") as f:
-        f.write(html_text)
-
-
-if __name__ == "__main__":
-    # Check the number of arguments
-    if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py <markdown_file> <output_file>", file=sys.stderr)
-        sys.exit(1)
-
-    # Get the Markdown file and output file names from the command line arguments
-    markdown_file = sys.argv[1]
-    output_file = sys.argv[2]
-
-    # Convert Markdown to HTML and write to the output file
-    convert_markdown_to_html(markdown_file, output_file)
-
-    # Print nothing and exit successfully
-    sys.exit(0)
-    
-    def convert_markdown_to_html(markdown_file, output_file):
-    # Check if the Markdown file exists
-    if not os.path.exists(markdown_file):
-        print(f"Missing {markdown_file}", file=sys.stderr)
-        sys.exit(1)
-
-    # Read the contents of the Markdown file
-    with open(markdown_file, "r") as f:
-        markdown_text = f.read()
-
-    # Convert Markdown to HTML
-    html_text = markdown.markdown(markdown_text)
-
-    # Parse headings and replace with HTML
-    html_text = html_text.replace("<p># ", "<h1>").replace(" #</p>", "</h1>")
-    html_text = html_text.replace("<p>## ", "<h2>").replace(" ##</p>", "</h2>")
-    html_text = html_text.replace("<p>### ", "<h3>").replace(" ###</p>", "</h3>")
-    html_text = html_text.replace("<p>#### ", "<h4>").replace(" ####</p>", "</h4>")
-    html_text = html_text.replace("<p>##### ", "<h5>").replace(" #####</p>", "</h5>")
-    html_text = html_text.replace("<p>###### ", "<h6>").replace(" ######</p>", "</h6>")
-
-    # Write the HTML output to a file
-    with open(output_file, "w") as f:
-        f.write(html_text)
-
-
-if __name__ == "__main__":
-    # Check the number of arguments
-    if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py <markdown_file> <output_file>", file=sys.stderr)
-        sys.exit(1)
-
-    # Get the Markdown file and output file names from the command line arguments
-    markdown_file = sys.argv[1]
-    output_file = sys.argv[2]
-
-    # Convert Markdown to HTML and write to the output file
-    convert_markdown_to_html(markdown_file, output_file)
-
-    # Print nothing and exit successfully
-    sys.exit(0)
-
-def convert_markdown_to_html(markdown_file, output_file):
-    # Check if the Markdown file exists
-    if not os.path.exists(markdown_file):
-        print(f"Missing {markdown_file}", file=sys.stderr)
-        sys.exit(1)
-
-    # Read the contents of the Markdown file
-    with open(markdown_file, "r") as f:
-        markdown_text = f.read()
-
-    # Convert Markdown to HTML
-    html_text = markdown.markdown(markdown_text)
-
-    # Parse headings and replace with HTML
-    html_text = html_text.replace("<p># ", "<h1>").replace(" #</p>", "</h1>")
-    html_text = html_text.replace("<p>## ", "<h2>").replace(" ##</p>", "</h2>")
-    html_text = html_text.replace("<p>### ", "<h3>").replace(" ###</p>", "</h3>")
-    html_text = html_text.replace("<p>#### ", "<h4>").replace(" ####</p>", "</h4>")
-    html_text = html_text.replace("<p>##### ", "<h5>").replace(" #####</p>", "</h5>")
-    html_text = html_text.replace("<p>###### ", "<h6>").replace(" ######</p>", "</h6>")
-
-    # Parse unordered lists and replace with HTML
-    html_text = html_text.replace("<ul>\n", "<ul>")
-    html_text = html_text.replace("</ul>\n", "</ul>")
-    html_text = html_text.replace("<li>", "<li>")
-    html_text = html_text.replace("</li>\n", "</li>")
-
-    # Write the HTML output to a file
-    with open(output_file, "w") as f:
-        f.write(html_text)
-
-
-if __name__ == "__main__":
-    # Check the number of arguments
-    if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py <markdown_file> <output_file>", file=sys.stderr)
-        sys.exit(1)
-
-    # Get the Markdown file and output file names from the command line arguments
-    markdown_file = sys.argv[1]
-    output_file = sys.argv[2]
-
-    # Convert Markdown to HTML and write to the output file
-    convert_markdown_to_html(markdown_file, output_file)
-
-    # Print nothing and exit successfully
-    sys.exit(0)
-
-def convert_markdown_to_html(markdown_file, output_file):
-    # Check if the Markdown file exists
-    if not os.path.exists(markdown_file):
-        print(f"Missing {markdown_file}", file=sys.stderr)
-        sys.exit(1)
-
-    # Read the contents of the Markdown file
-    with open(markdown_file, "r") as f:
-        markdown_text = f.read()
-
-    # Convert Markdown to HTML
-    html_text = markdown.markdown(markdown_text)
-
-    # Parse headings and replace with HTML
-    html_text = html_text.replace("<p># ", "<h1>").replace(" #</p>", "</h1>")
-    html_text = html_text.replace("<p>## ", "<h2>").replace(" ##</p>", "</h2>")
-    html_text = html_text.replace("<p>### ", "<h3>").replace(" ###</p>", "</h3>")
-    html_text = html_text.replace("<p>#### ", "<h4>").replace(" ####</p>", "</h4>")
-    html_text = html_text.replace("<p>##### ", "<h5>").replace(" #####</p>", "</h5>")
-    html_text = html_text.replace("<p>###### ", "<h6>").replace(" ######</p>", "</h6>")
-
-    # Parse unordered lists and replace with HTML
-    html_text = html_text.replace("<ul>\n", "<ul>")
-    html_text = html_text.replace("</ul>\n", "</ul>")
-    html_text = html_text.replace("<li>", "<li>")
-    html_text = html_text.replace("</li>\n", "</li>")
-
-    # Parse ordered lists and replace with HTML
-    html_text = html_text.replace("<ol>\n", "<ol>")
-    html_text = html_text.replace("</ol>\n", "</ol>")
-    html_text = html_text.replace("<li>", "<li>")
-    html_text = html_text.replace("</li>\n", "</li>")
-
-    # Write the HTML output to a file
-    with open(output_file, "w") as f:
-        f.write(html_text)
-
-
-if __name__ == "__main__":
-    # Check the number of arguments
-    if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py <markdown_file> <output_file>", file=sys.stderr)
-        sys.exit(1)
-
-    # Get the Markdown file and output file names from the command line arguments
-    markdown_file = sys.argv[1]
-    output_file = sys.argv[2]
-
-    # Convert Markdown to HTML and write to the output file
-    convert_markdown_to_html(markdown_file, output_file)
-
-    # Print nothing and exit successfully
-    sys.exit(0)
-
-def convert_markdown_to_html(markdown_file, output_file):
-    # Check if the Markdown file exists
-    if not os.path.exists(markdown_file):
-        print(f"Missing {markdown_file}", file=sys.stderr)
-        sys.exit(1)
-
-    # Read the contents of the Markdown file
-    with open(markdown_file, "r") as f:
-        markdown_text = f.read()
-
-    # Convert Markdown to HTML
-    html_text = markdown.markdown(markdown_text)
-
-    # Parse headings and replace with HTML
-    html_text = html_text.replace("<p># ", "<h1>").replace(" #</p>", "</h1>")
-    html_text = html_text.replace("<p>## ", "<h2>").replace(" ##</p>", "</h2>")
-    html_text = html_text.replace("<p>### ", "<h3>").replace(" ###</p>", "</h3>")
-    html_text = html_text.replace("<p>#### ", "<h4>").replace(" ####</p>", "</h4>")
-    html_text = html_text.replace("<p>##### ", "<h5>").replace(" #####</p>", "</h5>")
-    html_text = html_text.replace("<p>###### ", "<h6>").replace(" ######</p>", "</h6>")
-
-    # Parse unordered lists and replace with HTML
-    html_text = html_text.replace("<ul>\n", "<ul>")
-    html_text = html_text.replace("</ul>\n", "</ul>")
-    html_text = html_text.replace("<li>", "<li>")
-    html_text = html_text.replace("</li>\n", "</li>")
-
-    # Parse ordered lists and replace with HTML
-    html_text = html_text.replace("<ol>\n", "<ol>")
-    html_text = html_text.replace("</ol>\n", "</ol>")
-    html_text = html_text.replace("<li>", "<li>")
-    html_text = html_text.replace("</li>\n", "</li>")
-
-    # Parse paragraphs and replace with HTML
-    html_text = "<p>" + html_text.replace("\n\n", "</p>\n\n<p>") + "</p>"
-    html_text = html_text.replace("<p></p>", "")
-
-    # Write the HTML output to a file
-    with open(output_file, "w") as f:
-        f.write(html_text)
-
-
-if __name__ == "__main__":
-    # Check the number of arguments
-    if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py <markdown_file> <output_file>", file=sys.stderr)
-        sys.exit(1)
-
-    # Get the Markdown file and output file names from the command line arguments
-    markdown_file = sys.argv[1]
-    output_file = sys.argv[2]
-
-    # Convert Markdown to HTML and write to the output file
-    convert_markdown_to_html(markdown_file, output_file)
-
-    # Print nothing and exit successfully
-    sys.exit(0)
-    
-!/usr/bin/env python3
-import sys
 import re
 
-if len(sys.argv) < 3:
-    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-    sys.exit(1)
+if __name__ == '__main__':
 
-markdown_file = sys.argv[1]
-html_file = sys.argv[2]
+    # Test if the number of arguments passed is 2
+    if len(sys.argv[1:]) != 2:
+        print('Usage: ./markdown2html.py README.md README.html',
+              file=sys.stderr)
+        sys.exit(1)
 
-try:
-    with open(markdown_file, "r") as f:
-        markdown_text = f.read()
-except FileNotFoundError:
-    print(f"Missing {markdown_file}", file=sys.stderr)
-    sys.exit(1)
+    # Store the arguments into variables
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
 
-# Parse Headings
-markdown_text = re.sub(r'^(#+)\s+(.+)$', lambda m: f'<h{len(m.group(1))}>{m.group(2)}</h{len(m.group(1))}>', markdown_text, flags=re.MULTILINE)
+    # Check that the markdown file exist and it is a file
+    if not (os.path.exists(input_file) and os.path.isfile(input_file)):
+        print(f'Missing {input_file}', file=sys.stderr)
+        sys.exit(1)
 
-# Parse Unordered Lists
-markdown_text = re.sub(r'^\*\s+(.+)$', r'<li>\1</li>', markdown_text, flags=re.MULTILINE)
-markdown_text = re.sub(r'^([^\n]*)(\n\*)+', r'<ul>\n\0</ul>\n', markdown_text, flags=re.MULTILINE)
+    with open(input_file, encoding='utf-8') as file_1:
+        html_content = []
+        md_content = [line[:-1] for line in file_1.readlines()]
+        for line in md_content:
+            heading = re.split(r'#{1,6} ', line)
+            if len(heading) > 1:
+                # Compute the number of the # present to
+                # determine heading level
+                h_level = len(line[:line.find(heading[1])-1])
+                # Append the html equivalent of the heading
+                html_content.append(
+                    f'<h{h_level}>{heading[1]}</h{h_level}>\n'
+                )
+            else:
+                html_content.append(line)
 
-# Parse Ordered Lists
-markdown_text = re.sub(r'^\d+\.\s+(.+)$', r'<li>\1</li>', markdown_text, flags=re.MULTILINE)
-markdown_text = re.sub(r'^([^\n]*)(\n\d+\.)+', r'<ol>\n\0</ol>\n', markdown_text, flags=re.MULTILINE)
-
-# Parse Paragraphs
-markdown_text = re.sub(r'^([^\n]+)\n{2,}', r'<p>\1</p>\n', markdown_text, flags=re.MULTILINE)
-markdown_text = re.sub(r'__(.+?)__', r'<em>\1</em>', markdown_text, flags=re.MULTILINE)
-markdown_text = re.sub(r'\*\*(.+?)\*\*', r'<b>\1</b>', markdown_text, flags=re.MULTILINE)
-
-try:
-    with open(html_file, "w") as f:
-        f.write(markdown_text)
-except:
-    print(f"Error writing to {html_file}", file=sys.stderr)
-    sys.exit(1)
-
-sys.exit(0)
-
-#!/usr/bin/env python3
-
-import sys
-import re
-import hashlib
-
-if len(sys.argv) < 3:
-    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-    sys.exit(1)
-
-markdown_file = sys.argv[1]
-html_file = sys.argv[2]
-
-try:
-    with open(markdown_file, "r") as f:
-        markdown_text = f.read()
-except FileNotFoundError:
-    print(f"Missing {markdown_file}", file=sys.stderr)
-    sys.exit(1)
-
-# Parse Headings
-markdown_text = re.sub(r'^(#+)\s+(.+)$', lambda m: f'<h{len(m.group(1))}>{m.group(2)}</h{len(m.group(1))}>', markdown_text, flags=re.MULTILINE)
-
-# Parse Unordered Lists
-markdown_text = re.sub(r'^\*\s+(.+)$', r'<li>\1</li>', markdown_text, flags=re.MULTILINE)
-markdown_text = re.sub(r'^([^\n]*)(\n\*)+', r'<ul>\n\0</ul>\n', markdown_text, flags=re.MULTILINE)
-
-# Parse Ordered Lists
-markdown_text = re.sub(r'^\d+\.\s+(.+)$', r'<li>\1</li>', markdown_text, flags=re.MULTILINE)
-markdown_text = re.sub(r'^([^\n]*)(\n\d+\.)+', r'<ol>\n\0</ol>\n', markdown_text, flags=re.MULTILINE)
-
-# Parse Paragraphs
-markdown_text = re.sub(r'^([^\n]+)\n{2,}', r'<p>\1</p>\n', markdown_text, flags=re.MULTILINE)
-
-# Parse Bold
-markdown_text = re.sub(r'\*\*(.+?)\*\*', r'<b>\1</b>', markdown_text, flags=re.MULTILINE)
-
-# Parse MD5
-markdown_text = re.sub(r'\[\[(.+?)\]\]', lambda m: hashlib.md5(m.group(1).encode('utf-8')).hexdigest(), markdown_text)
-
-# Parse Remove C
-markdown_text = re.sub(r'\(\((.+?)\)\)', lambda m: m.group(1).replace('c', '').replace('C', ''), markdown_text)
-
-try:
-    with open(html_file, "w") as f:
-        f.write(markdown_text)
-except:
-    print(f"Error writing to {html_file}", file=sys.stderr)
-    sys.exit(1)
-
-sys.exit(0)
+    with open(output_file, 'w', encoding='utf-8') as file_2:
+        file_2.writelines(html_content)
